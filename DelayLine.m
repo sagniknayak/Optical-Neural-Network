5568fee14f1cfa6786ada07833af4c482b02e4eb
sagniknayak <sagniknayak26@gmail.com>
2023-03-03 22:55:27 +0530

    Added class MZM and Edited class DelayLine

--- DelayLine.m
+++ DelayLine.m
@@ -4,19 +4,20 @@ classdef DelayLine
         r;
         d;
         L;
+        lambda;
         c = 3e+8;
     end
     methods
-        function obj = DelayLine(n_in, r_in, d_in, L_in)
+        function obj = DelayLine(n_in, r_in, d_in, L_in, wavelength)
             obj.n = n_in;
             obj.r = r_in;
             obj.d = d_in;
             obj.L = L_in;
+            obj.lambda = wavelength;
         end
-        function E_out = compute(obj, E_in, lambda, V1, V2)
+        function E_out = compute(obj, E_in, V1, V2)
             n_eff = obj.n - 0.5*obj.r*(obj.n.^3)*((V1-V2)/obj.d);
-            phi = 2*pi*(1/lambda)*n_eff*obj.L;
-            v = exp(1j*phi);
+            phi = 2*pi*(1/obj.lambda)*n_eff*obj.L;
             E_out = exp(1j*phi) * E_in;  
         end
     end
---
+++ MZM.m
@@ -0,0 +1,22 @@
+classdef MZM
+    properties
+        dc1;
+        dl1;
+        dl2;
+        dc2;
+    end
+    methods
+        function obj = MZM(n, r, d, L, lambda, kab, kba, del, z)
+            obj.dc1 = DirectionalCoupler(kab,kba,del,z);
+            obj.dl1 = DelayLine(n,r,d,L,lambda);
+            obj.dl2 = DelayLine(n,r,d,L,lambda);
+            obj.dc2 = DirectionalCoupler(kab,kba,del,z);
+        end
+        function E_out = compute(obj, E_in, V1, G1, V2, G2)
+            E_dc1 = obj.dc1.compute(E_in);
+            E_dl1 = obj.dl1.compute(E_dc1(1), V1, G1);
+            E_dl2 = obj.dl2.compute(E_dc1(2), V2, G2);
+            E_out = obj.dc2.compute([E_dl1; E_dl2]);
+        end
+    end
+end
