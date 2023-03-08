9348a6419d267a0ad7f59c1af2cd5c880c5fa09b
sagniknayak <sagniknayak26@gmail.com>
2023-03-08 21:11:17 +0530

    Added voltage control to Sample Arm of MZM

--- MZM.m
+++ MZM.m
@@ -11,11 +11,19 @@ classdef MZM
             obj.dl1 = DelayLine(n,r,d,L,lambda);
             obj.dl2 = DelayLine(n,r,d,L,lambda);
             obj.dc2 = DirectionalCoupler(kab,kba,del,z);
+            obj.V1 = 0;
+            obj.G1 = 0;
+            obj.V2 = 0;
+            obj.G2 = 0;
         end
-        function E_out = compute(obj, E_in, V1, G1, V2, G2)
+        function set_voltage(obj, V1, G1)
+            obj.V1 = V1;
+            obj.G1 = G1;
+        end
+        function E_out = compute(obj, E_in)
             E_dc1 = obj.dc1.compute(E_in);
-            E_dl1 = obj.dl1.compute(E_dc1(1), V1, G1);
-            E_dl2 = obj.dl2.compute(E_dc1(2), V2, G2);
+            E_dl1 = obj.dl1.compute(E_dc1(1), obj.V1, obj.G1);
+            E_dl2 = obj.dl2.compute(E_dc1(2), obj.V2, obj.G2);
             E_out = obj.dc2.compute([E_dl1; E_dl2]);
         end
     end
